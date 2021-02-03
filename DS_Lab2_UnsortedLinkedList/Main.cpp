1c8bd8a532dbfa1681282390ab8ddd64980f4549
hankel343 <57358008+hankel343@users.noreply.github.com>
2021-02-03 06:22:49 -0600

    -Rebuilt Main.cpp from scratch to remove scoping issues with unionList.

--- DS_Lab2_UnsortedLinkedList/Main.cpp
+++ DS_Lab2_UnsortedLinkedList/Main.cpp
@@ -5,68 +5,88 @@
  *Method headers for the UnsortedList class are found in UnsortedList.h and their implementations in UnsortedList.cpp.
  *This program also makes use of a class called ItemType that allows for client software to use UnsortedList regardless of data that will be in the list.*/
 
-#include "UnsortedList.h"
 #include "ItemType.h"
+#include "UnsortedList.h"
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-void ListOperationsMenu(UnsortedList &activeList, UnsortedList &passiveList);
+void ListOperationsMenu(UnsortedList *list);
 //Pre: Memory should be allocated for two UnsortedList types and their locations should be passed as arguments.
 //Post: The memory address of the list to have operations performed on it is set to activeList.
 
-void ProcessListOperation(char selection, UnsortedList &activeList, UnsortedList &passiveList);
+void ProcessListOperation(UnsortedList *list, char selection);
 //Pre: Memory is allocated for two UnsortedList types and user input has been read into char identifier selection.
 //Post: Function will perform the specified operation on the activeList UnsortedList.
 
-string ConvertToUpperCase(string &userInput);
-//Pre: Memory has been allocated for a string identifier called userInput and user has entered information from keyboard.
-//Post: User keyboard input is converted to uppercase.
+void MakeUnion(UnsortedList *list1, UnsortedList *list2, UnsortedList *unionList);
+//Pre: Memory has been allocated for three unsorted list objects and the source lists are not empty.
+//Post: The target list has been populated with the unique elements of the two source lists.
 
 void ClientInputValidation(int &input);
 //Pre: Memory has been allocated for an integer identifier called input.
 //Post: User is prompted for input until valid input is provided.
 
+string ConvertToUpperCase(string& userInput);
+//Pre: Memory has been allocated for a string identifier called userInput and user has entered information from keyboard.
+//Post: User keyboard input is converted to uppercase.
+
 int main()
 {
-	UnsortedList list1, list2;
+	//Creating pointers to three lists.
+	UnsortedList *list1, *list2, *unionList;
+	//Populating each pointer with addresses of UnsortedLists from heap memory.
+	list1 = new UnsortedList;
+	list2 = new UnsortedList;
+	unionList = new UnsortedList;
 	string input;
 
 	do
 	{
-		cout << "This program has allocated memory for two empty unsorted linked lists.\n";
-		cout << "Their names are list1 and list2.\n";
-		cout << "Enter the name of the list (\"list1\" or \"list2\") that you would like to perform operations on or press 'Q' to quit.\n";
-		cout << "Enter the list name: ";
+		cout << "Memory has been allocated for two unsorted linked lists.\n";
+		cout << "Enter either \"list1\" or \"list2\" to access either list to perform operations on it.\n";
+		cout << "Once each list contains data, enter \"union\" to create a third list that is the union of list1 and list2.\n";
+		cout << "Enter 'Q' to quit.\n";
 		cin >> input;
-		ConvertToUpperCase(input);
+
 		system("cls");
 
-		//Determines which list will have operations performed on it.
+		ConvertToUpperCase(input);
+
 		if (input == "LIST1") {
-			ListOperationsMenu(list1, list2);
+			ListOperationsMenu(list1);
 		} else if (input == "LIST2") {
-			ListOperationsMenu(list2, list1);
-		} else {
-			cout << "Invalid input - Please try again.\n";
+			ListOperationsMenu(list2);
+		} else if (input == "UNION" && (list1->GetLength() == 0 || list2->GetLength() == 0)) {
+			cout << "Atleast one of the lists to perform the union on is empty.\nAdd elements to both before performing a union.\n";
+			cout << "**************************************************\n\n";
+		} else if (input == "UNION" && list1->GetLength() != 0 && list2->GetLength() != 0) {
+			MakeUnion(list1, list2, unionList);
+		} else if (input == "Q") {
+			cout << "Goodbye...\n";
+			break;
 		}
-		
+		else {
+			cout << "Unrecognized input - Try again.\n";
+			cout << "*******************************\n";
+		}
 	} while (input != "Q");
 	
-	system("cls");
-	cout << "Goodbye...\n";
-	
+	//Deallocating memory for each node in the lists.
+	list1->MakeEmpty();
+	list2->MakeEmpty();
+
+	//Deallocating memory for lists themselves.
+	delete list1, list2, unionList;
 
 	return 0;
 }
 
-void ListOperationsMenu(UnsortedList &activeList, UnsortedList &passiveList) {
-	char selection = NULL;
-
-	do 
+void ListOperationsMenu(UnsortedList* list)
+{
+	char selection;
+	do
 	{
-
 		cout << "\nEnter your selection from the menu below: \n\n";
 		cout << "1 - Insert an item into the list.\n";
 		cout << "2 - Search for an item in the list.\n";
@@ -75,17 +95,16 @@ void ListOperationsMenu(UnsortedList &activeList, UnsortedList &passiveList) {
 		cout << "5 - Make the list empty.\n";
 		cout << "6 - Reset the list to the first value.\n";
 		cout << "7 - Get the length of the list.\n";
-		cout << "8 - Union with a second list.\n";
 		cout << "B - Back.\n\n";
 		cout << "Enter your selection: ";
 		cin >> selection;
 		system("cls");
-		ProcessListOperation(selection, activeList, passiveList);
-
+		ProcessListOperation(list, selection);
 	} while (toupper(selection) != 'B');
 }
 
-void ProcessListOperation(char selection, UnsortedList& activeList, UnsortedList& passiveList) {
+void ProcessListOperation(UnsortedList* list, char selection)
+{
 	switch (toupper(selection)) {
 	case '1': {
 		ItemType item;
@@ -96,24 +115,25 @@ void ProcessListOperation(char selection, UnsortedList& activeList, UnsortedList
 		}
 
 		item.Set(inputItem);
-		activeList.InsertItem(item);
+		list->InsertItem(item);
 
 		break;
 	} case '2': {
 		ItemType item;
-		Node *returnValue; //Stores the returned node address from the search.
+		Node* returnValue; //Stores the returned node address from the search.
 		int inputItem;
 		cout << "Enter the item that you would like to search the list for: ";
 		if (!(cin >> inputItem)) {
 			ClientInputValidation(inputItem);
 		}
 
-		item.Set(inputItem); 
-		returnValue = activeList.Search(item); //Search method called (returns an address if found, null if not found)
+		item.Set(inputItem);
+		returnValue = list->Search(item); //Search method called (returns an address if found, null if not found)
 
 		if (returnValue == NULL) {
 			cout << inputItem << " was not found in the list\n\n";
-		} else {
+		}
+		else {
 			cout << returnValue->data.Get() << " was found in the list\n\n";
 		}
 
@@ -128,63 +148,43 @@ void ProcessListOperation(char selection, UnsortedList& activeList, UnsortedList
 		}
 		item.Set(inputItem);
 
-		activeList.DeleteItem(item, found);
+		list->DeleteItem(item, found);
 		break;
 
 	} case '4': {
-		if (activeList.GetLength() == 0) { //If the list is empty the GetNextItem() method is not called.
+		if (list->GetLength() == 0) { //If the list is empty the GetNextItem() method is not called.
 			cout << "The list is currently empty.\n";
-		} else {
-			cout << "The next item in the list is: " << activeList.GetNextItem().Get() << endl;;
+		}
+		else {
+			cout << "The next item in the list is: " << list->GetNextItem().Get() << endl;;
 		}
 		break;
 	} case '5': {
-		activeList.MakeEmpty();
+		list->MakeEmpty();
 		break;
 	} case '6': {
-		activeList.ResetList();
+		list->ResetList();
 		break;
 	} case '7': {
-		cout << "The current length of the list is: " << activeList.GetLength() << endl;
+		cout << "The current length of the list is: " << list->GetLength() << endl;
 		break;
-	} case '8': {
-		//Following pointers are declared for the union operation so the destructor is not called on the active or passive list at the end of the method.
-		UnsortedList *unionListPtr = new UnsortedList;
-		UnsortedList *activeListPtr = &activeList;
-		UnsortedList* passiveListPtr = &passiveList;
-
-		if (activeList.GetLength() == 0 || passiveList.GetLength() == 0) {
-			cout << "One of the lists is empty.\n";
-		} else {
-			cout << "Union of these lists: ";
-			//Union returns a pointer, so this simply calls the PrintList() method on the returned pointer to an UnsortedList type.
-			activeList.Union(unionListPtr, activeListPtr, passiveListPtr)->PrintList();
-			cout << endl;
-		}
-
-		unionListPtr->MakeEmpty(); //Releases memory allocated for each node in the list.
-		delete unionListPtr; //Releases memory allocated for the third list.
-		break;
-	} case '9': {
-		cout << "Contents of the other list: \n";
-		passiveList.PrintList();
-		break;
-	} case 'B': {
+	}  case 'B': {
 		break;
 	} default: {
 		cout << "Unrecognized input - please try again.\n";
-	}
-	
-	}
+	} //default case bracket
+
+	}//End of switch block bracket
 }
 
-string ConvertToUpperCase(string& userInput) {
-	
-	for (int i = 0; i < userInput.length(); i++) {
-		userInput[i] = toupper(userInput[i]);
-	}
-
-	return userInput;
+void MakeUnion(UnsortedList* list1, UnsortedList* list2, UnsortedList *unionList)
+{
+	unionList = list1->Union(unionList, list1, list2);
+	cout << "The unique elements of list1 and list2 have been combined to create a third union list.\n";
+	cout << "The menu below allows you to perform list operations on this new list.\n";
+	cout << "**********************************************************************\n";
+	ListOperationsMenu(unionList);
+	unionList->MakeEmpty(); //Deallocates memory from nodes of the unionList when user is done with it.
 }
 
 void ClientInputValidation(int& input) {
@@ -194,4 +194,13 @@ void ClientInputValidation(int& input) {
 		cout << "\aInput for this program should an integer - please try again: ";
 		cin >> input;
 	}
+}
+
+string ConvertToUpperCase(string& userInput) {
+
+	for (int i = 0; i < userInput.length(); i++) {
+		userInput[i] = toupper(userInput[i]);
+	}
+
+	return userInput;
 }
